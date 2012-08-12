27d5db3569a3fe6027b2c3d8cae7207e70dc5061
Rhys Ulerich <rhys.ulerich@gmail.com>
2012-08-12 17:09:22 -0500

    Update dox

--- search/all_73.js
+++ search/all_73.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['strided_5fadaptor',['strided_adaptor',['../classar_1_1strided__adaptor.html',1,'ar']]],
   ['sum_5ferror',['sum_error',['../structsum__error.html',1,'']]]
 ];
