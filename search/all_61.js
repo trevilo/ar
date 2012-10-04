0aca636cc659cb4447938322a1695055a996f355
Rhys Ulerich <rhys.ulerich@gmail.com>
2012-10-04 00:51:23 -0500

    Update Doxygen

--- search/all_61.js
+++ search/all_61.js
@@ -5,6 +5,7 @@ var searchData=
   ['ar',['ar',['../namespacear.html',1,'']]],
   ['ar_2ehpp',['ar.hpp',['../ar_8hpp.html',1,'']]],
   ['arcov_2doctfile_2ecpp',['arcov-octfile.cpp',['../arcov-octfile_8cpp.html',1,'']]],
+  ['arg',['Arg',['../structArg.html',1,'']]],
   ['arsel_2doctfile_2ecpp',['arsel-octfile.cpp',['../arsel-octfile_8cpp.html',1,'']]],
   ['arsel_2ecpp',['arsel.cpp',['../arsel_8cpp.html',1,'']]],
   ['autocorrelation',['autocorrelation',['../namespacear.html#a7a5016706d3db6d6b5d8c8cf9939dcfb',1,'ar']]]
--- search/classes_61.js
+++ search/classes_61.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['aic',['AIC',['../structar_1_1AIC.html',1,'ar']]],
-  ['aicc',['AICC',['../structar_1_1AICC.html',1,'ar']]]
+  ['aicc',['AICC',['../structar_1_1AICC.html',1,'ar']]],
+  ['arg',['Arg',['../structArg.html',1,'']]]
 ];
