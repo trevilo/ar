ad80836d24e0434e3cb7b5c186ccd343988641b6
Rhys Ulerich <rhys.ulerich@gmail.com>
2012-12-20 17:00:41 -0600

    update Dox

---
+++ search/classes_65.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['empirical_5fvariance_5ffunction',['empirical_variance_function',['../structar_1_1empirical__variance__function.html',1,'ar']]],
+  ['empirical_5fvariance_5fgenerator',['empirical_variance_generator',['../classar_1_1empirical__variance__generator.html',1,'ar']]],
+  ['empirical_5fvariance_5fiterator',['empirical_variance_iterator',['../classar_1_1empirical__variance__iterator.html',1,'ar']]],
+  ['estimation_5fmethod',['estimation_method',['../structar_1_1estimation__method.html',1,'ar']]]
+];
---
+++ search/classes_70.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['predictor',['predictor',['../classar_1_1predictor.html',1,'ar']]]
+];
